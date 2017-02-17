b9d216531cc2731e633ab6ea381b253d479e84f5
alexzaitzev <mail@alexzaitzev.pro>
2017-02-17 17:21:35 +0300

    Added draft employee card. Added support es6 and refactor service with no fake data.

--- app/employee.service.ts
+++ app/employee.service.ts
@@ -1,32 +1,62 @@
+import { Injectable } from 'angular2/core';
+import { Http, Response, HTTP_PROVIDERS } from 'angular2/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
 export class EmployeeService
 {
-    getEmployee() : Employee[]
+    private employeeUrl = 'https://virtserver.swaggerhub.com/alexzaitzev/stages/1.0.2/floors/10';
+
+    constructor (private http: Http) {}
+
+    getEmployee() : Observable<Employee[]>
     {
-        return [
-            {
-                Id: 1,
-                Photo: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTWQd6S3Ua282NvPgUIiY_xuI49vjw1wsnEhxbl8OWUanJ4gbg8",
-                Location: { X: 344, Y: 197}
-            },
-            {
-                Id: 2,
-                Photo: "https://blog.interviewmocha.com/wp-content/uploads/2016/01/Scott-Hanselman-150x150.jpeg",
-                Location: { X: 479, Y: 197}
-            },
-            {
-                Id: 3,
-                Photo: "https://pbs.twimg.com/profile_images/378800000550413574/1bda93983b282cc4572e4ae7f1fae3f4_400x400.jpeg",
-                Location: { X: 623, Y: 197}
-            }
-        ];
+        return this.http.get(this.employeeUrl)
+                        .map(this.mapData);
+
+        // return [
+        //     {
+        //         Id: 1,
+        //         Photo: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTWQd6S3Ua282NvPgUIiY_xuI49vjw1wsnEhxbl8OWUanJ4gbg8",
+        //         Location: { X: 344, Y: 197}
+        //     },
+        //     {
+        //         Id: 2,
+        //         Photo: "https://blog.interviewmocha.com/wp-content/uploads/2016/01/Scott-Hanselman-150x150.jpeg",
+        //         Location: { X: 479, Y: 197}
+        //     },
+        //     {
+        //         Id: 3,
+        //         Photo: "https://pbs.twimg.com/profile_images/378800000550413574/1bda93983b282cc4572e4ae7f1fae3f4_400x400.jpeg",
+        //         Location: { X: 623, Y: 197}
+        //     }
+        // ];
+    }
+
+    private mapData(res: Response) {
+        let people = res.json().employees.map(x => new Employee(x));
+
+        return people || { };
     }
 }
 
 export class Employee
 {
+    constructor(o) {
+        this.Id = o.id;
+        this.Photo = o.photo;
+        this.Location = o.location;
+        this.Opened = false;
+    }
+
     Id: number;
     Photo: string;
-    Location: Coordinates
+    Location: Coordinates;
+    Opened: boolean;
 }
 
 export class Coordinates
--- app/floors.component.ts
+++ app/floors.component.ts
@@ -1,26 +1,31 @@
-import {Component} from 'angular2/core'
-import {EmployeeService} from './employee.service'
-import {AutoGrowDirective} from './auto-grow.directive'
+import {Component}          from 'angular2/core'
+import {EmployeeService}    from './employee.service'
+import {AutoGrowDirective}  from './auto-grow.directive'
+import {HTTP_PROVIDERS}     from 'angular2/http';
 
 @Component({
     selector: 'floors',
     styleUrls: ['app/styles.css'],
     templateUrl: 'app/templates/floor.template.html',
-    providers: [EmployeeService],
+    providers: [EmployeeService, HTTP_PROVIDERS],
     directives: [AutoGrowDirective]
 })
 
 export class FloorsComponent
 {
     backgroundImg: string = "https://s-media-cache-ak0.pinimg.com/originals/86/71/9f/86719f83b01c5e253426be7078658c1a.jpg";
-    employees;
+    employees = [];
+    errorMessage;
 
-    onFaceClick(event) {
-        console.log(event.target.dataset.id);
+    onFaceClick(o) {
+        this.employees.filter(e => e.Id === o.Id)[0].Opened = !o.Opened;
+        console.log(o);
     }
 
     constructor(employeeService: EmployeeService)
     {
-        this.employees = employeeService.getEmployee();
+        employeeService
+            .getEmployee()
+            .subscribe(x => this.employees = x);
     }
 }
